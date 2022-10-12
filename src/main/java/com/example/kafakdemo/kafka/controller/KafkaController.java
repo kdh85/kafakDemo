ab511e9753937f90baaad363b8c75c8ea93b0df7
kdh85 <xerodin85@gmail.com>
2022-10-12 13:50:03 +0900

    feat : 호출 controller 생성.

--- src/main/java/com/example/kafakdemo/kafka/controller/KafkaController.java
+++ src/main/java/com/example/kafakdemo/kafka/controller/KafkaController.java
@@ -1,2 +1,33 @@
-package com.example.kafakdemo.kafka.controller;public class KafkaController {
+package com.example.kafakdemo.kafka.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kafakdemo.kafka.dto.RequestDto;
+import com.example.kafakdemo.kafka.service.KafkaProducerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(value = "kafka")
+@RequiredArgsConstructor
+public class KafkaController {
+	private final KafkaProducerService producer;
+
+	@PostMapping
+	public String sendMessage(@RequestParam("message") String message) {
+		this.producer.sendMessage(message);
+
+		return "success";
+	}
+
+	@PostMapping("/test")
+	public String sendDto(@RequestBody RequestDto requestDto) throws JsonProcessingException {
+		this.producer.sendMessage(requestDto.toJson());
+		return "success";
+	}
 }
